c21c679d71e7a3b4f30eaf2c97fb690d688df4ea
Carlos Gabriel <carloscms2009@gmail.com>
2021-02-20 20:12:27 -0300

    Adicionando Suporte CORs.

--- src/main/java/com/carlosgabriel/cnpjcpfservice/DatabaseConfig.java
+++ src/main/java/com/carlosgabriel/cnpjcpfservice/DatabaseConfig.java
@@ -1,9 +1,9 @@
 package com.carlosgabriel.cnpjcpfservice;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
+//import com.zaxxer.hikari.*;
+//import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
-import javax.sql.DataSource;
+// javax.sql.DataSource;
 
 /**
  * Configurações de Banco de Dados no Heroku
@@ -14,14 +14,14 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
-
-  @Bean
-  public DataSource dataSource() {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-  }
+//  @Value("${spring.datasource.url}")
+//  private String dbUrl;
+//
+//  @Bean
+//  public DataSource dataSource() {
+//      HikariConfig config = new HikariConfig();
+//      config.setJdbcUrl(dbUrl);
+//      return new HikariDataSource(config);
+//  }
   
 }
